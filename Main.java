b0c5e04b9f36b28693c6bf475f4139c15015ba6a
bvpotapenko <bvpotapenko@gmail.com>
2018-11-27 00:23:55 +0800

    Доработаны тесты JUnit

--- Main.java
+++ Main.java
@@ -15,17 +15,17 @@ public class Main {
         String str = "String";
 
         //Tests:
-        System.out.println("Task 3: решить выражение a * (b + (c / d)); 5 * (6 + (7 / 9)) =  "+ task3_expressionSolver(5, 6, 7, 9));
+        System.out.println("Task 3: решить выражение a * (b + (c / d)); 5 * (6 + (7 / 9)) =  "+ task3_solveExpression(5, 6, 7, 0));
         System.out.println();
 
-        System.out.println("Task 4: проверить, что сумма от 10 до 20. 3+12 "+ task4_isSummFrom10To20(3, 12));
-        System.out.println("Task 4: проверить, что сумма от 10 до 20 5+4 "+ task4_isSummFrom10To20(5, 4));
+        System.out.println("Task 4: проверить, что сумма от 10 до 20. 3+12 "+ task4_isSumFrom10to20(3, 12));
+        System.out.println("Task 4: проверить, что сумма от 10 до 20 5+4 "+ task4_isSumFrom10to20(5, 4));
         System.out.println();
 
         System.out.println("Task 5: положительное или отрицательное? 10: ");
-        task5_isPosiriveOrNegative(10);
+        task5_isPosOrNeg(10);
         System.out.println("Task 5: положительное или отрицательное? -10: ");
-        task5_isPosiriveOrNegative(-10);
+        task5_isPosOrNeg(-10);
         System.out.println();
 
         System.out.println("Task 6: является отрицательным? -1 "+ task6_isNegative(-1));
@@ -33,9 +33,9 @@ public class Main {
         System.out.println();
 
         System.out.println("Task 7: Поздороваться с Иваном ");
-        task7_hello("Иван");
+        task7_printHello("Иван");
         System.out.println("Task 7: Поздороваться со Степаном ");
-        task7_hello("Степан");
+        task7_printHello("Степан");
         System.out.println();
 
         System.out.println("Task 8: проверить на високосность года");
@@ -52,21 +52,21 @@ public class Main {
 
     //Task 3: Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
     //где a, b, c, d – входные параметры этого метода;
-    public static float task3_expressionSolver(int a, int b, int c, int d){
+    public static float task3_solveExpression(int a, int b, int c, int d){
             return a * (b + ((float)c / d)); //приводим один из операндов к типу float,
                                             // чтоб значение всего выражения не округлилось
     }
 
     //Task 4: Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в
     //пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false;
-    public static boolean task4_isSummFrom10To20(int a, int b){
+    public static boolean task4_isSumFrom10to20(int a, int b){
         return (a + b) >= 10 && (a + b) <= 20; // СУММА больше 10 И СУММА меньше 20 Да/Нет?
     }
 
     //Task 5. Написать метод, которому в качестве параметра передается целое число, метод должен
     //напечатать в консоль положительное ли число передали, или отрицательное; Замечание:
     //ноль считаем положительным числом.
-    public static void task5_isPosiriveOrNegative(int i) {
+    public static void task5_isPosOrNeg(int i) {
         //Если параметр меньше нуля, то выведем [i] is negative, иначе [i] is positive
         System.out.println(i < 0 ? i+" is negative" : i + " is positive");
     }
@@ -81,7 +81,7 @@ public class Main {
     //Task 7. Написать метод, которому в качестве параметра передается строка, обозначающая имя,
     //метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
 
-    public static void task7_hello(String name){
+    public static void task7_printHello(String name){
         System.out.println("Привет, " + name + "!");
     }
 
@@ -89,8 +89,10 @@ public class Main {
     //консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й –
     //високосный.
     public static void task8_isLeapYear(int year) {
-        if(( year % 4 == 0 && year % 100 != 0) ||
-                ( year % 400 == 0)){
+        if(year >0 && //год больше нуля и делится на 4, но не делится на 100; или год положительный и делится на 400.
+                (( year % 4 == 0 && year % 100 != 0) ||
+                ( year % 400 == 0))
+            ){
             System.out.println(year + " - является високосным");
         }else
             System.out.println(year + " - не високосный");
--- MainTest.java
+++ MainTest.java
@@ -1,5 +1,223 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 class MainTest {
+
+    @Test
+    void task3_expressionSolver_CORRECT_ARGS_FLOAT_RETURNED() {
+        //Expected data
+        float expected1 = 33.88889f;
+        float expected2 = 2.75f;
+
+        //Actual result
+        float actual1 = Main.task3_solveExpression(5, 6, 7, 9); //33.88889
+        float actual2 = Main.task3_solveExpression(1, 2, 3, 4); //2.75
+
+
+        //Testing
+        Assertions.assertEquals(expected1, actual1, 0.000001f);
+        Assertions.assertEquals(expected2, actual2, 0.000001f);
+    }
+
+    @Test
+    void task3_expressionSolver_DEVIDE_BY_ZERO_Infinity_RETURNED() {
+        //Expected data
+        float expected = Float.valueOf("Infinity");
+
+        //Actual result
+        float actual = Main.task3_solveExpression(4, 10, 47, 0); //NaN
+
+        //Testing
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void task4_isSumFrom10to20_3_12_TRUE_RETURNED() {
+        Assertions.assertTrue(Main.task4_isSumFrom10to20(3, 12));
+    }
+
+    @Test
+    void task4_isSumFrom10to20_5_4_FALSE_RETURNED() {
+        Assertions.assertFalse(Main.task4_isSumFrom10to20(5, 4));
+    }
+
+    @Test
+    void task5_isPosOrNeg_10_IS_POSITIVE_RETURNED() {
+        //Expected
+        String expected = "10 is positive\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //Actual
+        Main.task5_isPosOrNeg(10);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task5_isPosOrNeg_neg10_IS_NEGATIVE_RETURNED() {
+        //Expected
+        String expected = "-10 is negative\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //Actual
+        Main.task5_isPosOrNeg(-10);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+    @Test
+    void task6_isNegative_neg1_TRUE_RETURNED() {
+        Assertions.assertTrue(Main.task6_isNegative(-1));
+    }
+    @Test
+    void task6_isNegative_7_FALSE_RETURNED() {
+        Assertions.assertFalse(Main.task6_isNegative(7));
+    }
+
+    @Test
+    void task7_printHello_HELLO_IVAN_RETURNED() {
+        //Expected
+        String expected = "Привет, Иван!\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task7_printHello("Иван");
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task7_printHello_HELLO_STEPAN_RETURNED() {
+        //Expected
+        String expected = "Привет, Степан!\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task7_printHello("Степан");
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_1600_LEAP_RETURNED() {
+        //Expected
+        String expected = "1600 - является високосным\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(1600);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_1500_NOT_LEAP_RETURNED() {
+        //Expected
+        String expected = "1500 - не високосный\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(1500);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_1604_LEAP_RETURNED() {
+        //Expected
+        String expected = "1604 - является високосным\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(1604);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_2100_NOT_LEAP_RETURNED() {
+        //Expected
+        String expected = "2100 - не високосный\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(2100);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_2000_LEAP_RETURNED() {
+        //Expected
+        String expected = "2000 - является високосным\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(2000);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_0_NOT_LEAP_RETURNED() {
+        //Expected
+        String expected = "0 - не високосный\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(0);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_100_NOT_LEAP_RETURNED() {
+        //Expected
+        String expected = "100 - не високосный\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(100);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    void task8_isLeapYear_2018_NOT_LEAP_RETURNED() {
+        //Expected
+        String expected = "2018 - не високосный\r\n";
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        //actual
+        Main.task8_isLeapYear(2018);
+
+        Assertions.assertEquals(expected, outContent.toString());
+    }
 
 }
