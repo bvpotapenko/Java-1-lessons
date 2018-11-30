aaceef931c9ea5bdd9cede720dfbb17590574194
bvpotapenko <bvpotapenko@gmail.com>
2018-11-30 15:09:40 +0800

    Сделана задача 7. Работает без доп массива. Используется только одна дополнительная свап-переменная.
    На юнит-тесты забил. Время дороже.

--- src/main/java/ru/bvpotapenko/se/Homework2.java
+++ src/main/java/ru/bvpotapenko/se/Homework2.java
@@ -32,7 +32,29 @@ public class Homework2 {
         findBalanceInArray(new int[]{});
 
         //Задача 7. Сдвинуть массив на N позиций
-        System.out.println(shiftArray(new ArrayList<Object>(Arrays.asList(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})), 3));
+        Integer[] testArr1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Shift = 3");
+        System.out.println(Arrays.toString(shiftArray(testArr1, 3)));
+
+        System.out.println("Shift = 1");
+        testArr1 = Arrays.stream(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ).boxed().toArray( Integer[]::new );
+        System.out.println(Arrays.toString(shiftArray(testArr1, 1)));
+
+        System.out.println("Shift = 5");
+        testArr1 = Arrays.stream(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ).boxed().toArray( Integer[]::new );
+        System.out.println(Arrays.toString(shiftArray(testArr1, 5)));
+
+        System.out.println("Shift = 23");
+        testArr1 = Arrays.stream(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ).boxed().toArray( Integer[]::new );
+        System.out.println(Arrays.toString(shiftArray(testArr1, 23)));
+
+        System.out.println("Shift = -1");
+        testArr1 = Arrays.stream(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ).boxed().toArray( Integer[]::new );
+        System.out.println(Arrays.toString(shiftArray(testArr1, -1)));
+
+        System.out.println("Shift = -3");
+        testArr1 = Arrays.stream(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} ).boxed().toArray( Integer[]::new );
+        System.out.println(Arrays.toString(shiftArray(testArr1, -3)));
     }
 
     //Читаем с консоли размер будущего массива
@@ -140,29 +162,48 @@ public class Homework2 {
         return sb.toString();
     }
 
-    //Позитивный сдвиг
-    public static List<Object> shiftArray (ArrayList<Object> arr, int shift){
-        if (arr.isEmpty() || shift == 0) return arr;
-        System.out.println(arr.toString());
-        for (int i = 0; i < shift; i++) {
-            arr = putItemInArray(
-                    putItemInArray(
-                            arr,
-                            arr.get(arr.size()-2),
-                            arr.size()-1),
-                    arr.get(arr.size()-1),
-                    0);
+    public static Object[] shiftArray(Object[] arr, int shift) {
+        if (arr.length == 0 || shift == 0) return arr;
+        Object tempItem; //swap-var
+        int tempItemIndex; //original position of swap-var in the array
+        int newPosition; // A new index for current item
+        int arrSize = arr.length;
+        int counterItemsShifted = 0; //Amount of array items being shifted already
+        int currentPosition = 0; //A position in the array we process right now
+
+        if (shift < 0) {
+            shift = arrSize + shift; //Заменяем отрицательное смещение циклическим положительным
+        } else {
+            shift = shift > arrSize ? shift%arrSize : shift; //сокращаем количество полных колец сдвига
         }
-        System.out.println(arr.toString());
+
+        while (counterItemsShifted < arrSize) {
+            //Step 1. Новая итерация,  temp  чист
+            newPosition = currentPosition + shift;
+            tempItem = arr[newPosition]; //Saved an item from the destination position...
+            tempItemIndex = newPosition; //...and put there in item from current
+            arr[newPosition] = arr[currentPosition];
+            counterItemsShifted++;
+
+            //Step 2. продолжаем итерацию с текущим temp
+            int fromPosition = currentPosition - shift < 0 ? arrSize - shift + currentPosition : currentPosition - shift;
+            int emptyIndex = currentPosition;
+            while (fromPosition != tempItemIndex) { //while we didn't come to the original position of the tempItem...
+                arr[emptyIndex] = arr[fromPosition];//...we continue to shift items
+                counterItemsShifted++;
+
+                emptyIndex = fromPosition; //remember new empty item
+                //Вычисляем ячейку-донор для новой свободной ячейки
+                fromPosition = fromPosition - shift < 0 ? arrSize - shift + fromPosition : fromPosition - shift;
+            }
+            arr[emptyIndex] = tempItem;
+            counterItemsShifted++;
+            if (currentPosition + 1 < arrSize) currentPosition++;
+            else break; //we managed to go through all the array
+
+        }
+
         return arr;
     }
- //   метод: положить в ячейку массива значение, вернуть массив. но перед складыванием рекурсивно перекинуть предыдущий
-     private static ArrayList<Object> putItemInArray(ArrayList<Object> arr, Object item, int position){
-        if (position -1 > 0){
-            arr.set(position, item);
-            putItemInArray(arr, arr.get(position-2), position-1);
-        }
-        return arr;
-     }
 
 }
