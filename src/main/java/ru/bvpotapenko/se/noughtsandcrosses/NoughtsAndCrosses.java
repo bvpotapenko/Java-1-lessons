aa587d371c47e924ac64962ba9b81627bff89931
bvpotapenko <bvpotapenko@gmail.com>
2018-12-02 12:08:39 +0800

    COde reformat

--- src/main/java/ru/bvpotapenko/se/noughtsandcrosses/NoughtsAndCrosses.java
+++ src/main/java/ru/bvpotapenko/se/noughtsandcrosses/NoughtsAndCrosses.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
 public class NoughtsAndCrosses {
     private static Scanner sc = new Scanner(System.in);
@@ -19,7 +18,7 @@ public class NoughtsAndCrosses {
     private static final String DRAW_GAME_MESSAGE = "Ничья!";
     private static final String USER_WON_MESSAGE = "Победил человек!";
     private static final String ROBOT_WON_MESSAGE = "Победил бот!";
-    private static final String ROBOT_PLAYED_MESSAGE =  "  ۜ\\(סּںסּَ` )/ۜ Бот походил в клетку: ";
+    private static final String ROBOT_PLAYED_MESSAGE = "  ۜ\\(סּںסּَ` )/ۜ Бот походил в клетку: ";
     private static final String INPUT_COORDINATES_MESSAGE = "Введите координаты хода через пробел: x y";
     private static final String SAD_ROBOT = "     ___T_\n" +
             "    | d b |\n" +
@@ -36,7 +35,7 @@ public class NoughtsAndCrosses {
             "    |_|_|\n" +
             "    /_|_\\";
 
-    static void initMap(){
+    static void initMap() {
         map = new char[mapWidth][mapHeight];
         for (int i = 0; i < mapWidth; i++) {
             for (int j = 0; j < mapHeight; j++) {
@@ -45,13 +44,13 @@ public class NoughtsAndCrosses {
         }
     }
 
-    static void printMap(){
+    static void printMap() {
         for (int k = 0; k < mapWidth + 1; k++) {
             System.out.printf(" %2d", k);
         }
         System.out.println();
         for (int i = 0; i < mapWidth; i++) {
-            System.out.printf(" %2d", i+1);
+            System.out.printf(" %2d", i + 1);
             for (int j = 0; j < mapHeight; j++) {
                 System.out.printf(" %2c", map[i][j]);
             }
@@ -59,35 +58,36 @@ public class NoughtsAndCrosses {
         }
     }
 
-    private static void playerTurn(){
+    private static void playerTurn() {
         int x = -1;
         int y = -1;
         do {
             System.out.println(INPUT_COORDINATES_MESSAGE);
             x = sc.nextInt() - 1;
             y = sc.nextInt() - 1;
-        }while (!isCellEmpty(x, y));
+        } while (!isCellEmpty(x, y));
         map[x][y] = DOT_PLAYER;
 
     }
 
-    private static boolean isCellEmpty(int x, int y){
+    private static boolean isCellEmpty(int x, int y) {
         if (x < 0 || y < 0 || x > mapWidth - 1 || y > mapHeight - 1)
             return false;
         return map[x][y] == DOT_EMPTY;
     }
-    private static void aiTurn(){
+
+    private static void aiTurn() {
         int x, y;
         do {
             x = random.nextInt(3);
             y = random.nextInt(3);
-        }while (!isCellEmpty(x, y));
+        } while (!isCellEmpty(x, y));
         map[x][y] = DOT_BOT;
         System.out.println(ROBOT_PLAYED_MESSAGE + x + " " + y);
 
     }
 
-    static boolean isMapFull(){
+    static boolean isMapFull() {
         for (int i = 0; i < mapWidth; i++) {
             for (int j = 0; j < mapHeight; j++) {
                 if (map[i][j] == DOT_EMPTY)
@@ -97,7 +97,7 @@ public class NoughtsAndCrosses {
         return true;
     }
 
-    private static boolean checkWin (char dot){
+    private static boolean checkWin(char dot) {
         if (map[0][0] == dot && map[0][1] == dot && map[0][2] == dot)
             return true;
         if (map[1][0] == dot && map[1][1] == dot && map[1][2] == dot)
@@ -119,7 +119,7 @@ public class NoughtsAndCrosses {
         return false;
     }
 
-    private static int[][] aiLogic(){
+    private static int[][] aiLogic() {
         //TODO:
         //Проверка угрозы поражения
         //проверить линии на наличие двух "Х"
@@ -136,36 +136,37 @@ public class NoughtsAndCrosses {
         //1. Ставить в центр, если он свободен
         //2. Ставя "О" в пустую линию или линию с 1х "Х", выбирать ячейку рядом с "Х".
         //3. Искать выгодные пересечения строк, в которых есть "О"
-    return null;
+        return null;
+
     }
 
 
-    public static void main( String[] args ){
+    public static void main(String[] args) {
         initMap();
         printMap();
 
-        while(true) {
+        while (true) {
             playerTurn();
             System.out.println();
             printMap();
-            if(checkWin(DOT_PLAYER)){
+            if (checkWin(DOT_PLAYER)) {
                 System.out.println(USER_WON_MESSAGE);
                 System.out.println(SAD_ROBOT);
                 break;
             }
-            if(isMapFull()){
+            if (isMapFull()) {
                 System.out.println(DRAW_GAME_MESSAGE);
                 break;
             }
             aiTurn();
-            if(checkWin(DOT_BOT)){
+            if (checkWin(DOT_BOT)) {
                 System.out.println(ROBOT_WON_MESSAGE);
                 System.out.println(HAPPY_ROBOT);
                 break;
             }
             System.out.println();
             printMap();
-            if(isMapFull()){
+            if (isMapFull()) {
                 System.out.println(DRAW_GAME_MESSAGE);
                 break;
             }
