7eb0726d17be3e197b5be037f3e71ab3a5ae6c09
bvpotapenko <bvpotapenko@gmail.com>
2019-02-02 00:02:22 +0800

    Updated tasks

--- src/main/java/ru/bvpotapenko/se/j1/l1/Main.java
+++ src/main/java/ru/bvpotapenko/se/j1/l1/Main.java
@@ -1,5 +1,8 @@
 package ru.bvpotapenko.se.j1.l1;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class Main {
 
     //Task 1: Создать пустой проект в IntelliJ IDEA и прописать метод main();
@@ -50,7 +53,55 @@ public class Main {
         task8_isLeapYear(4000);
         task8_isLeapYear(2400);
         task8_isLeapYear(2018);
+
+        System.out.println("Task 9: доказать (!(a && b) && (a || b)) || ((a && b) || !(a || b)) = true");
+        System.out.println("false || (a && !b) || (!a && b) || (a && b) || (!a && !b)");
+        System.out.println("Для любой пары значений а и б в выражении найдётся одна конъюнкция обращающаяся в истину.");
+        task9_isTrue(false, false);
+        task9_isTrue(true, true);
+        task9_isTrue(true, false);
+        task9_isTrue(false, true);
+
+        System.out.println("Task 10: проверить делимость А на Б");
+        task10_checkВivisibility(10, 5);
+        task10_checkВivisibility(5, 10);
+
+        System.out.println("Переменные a и b типа int. Упростите выражение (!(a < b) && !(a > b)).");
+        System.out.println("Упрощается до (a == b) по закону де Моргана");
+
+        System.out.println("Task 11: Проверить три числа (стороны трегольника)");
+        task11_checkThreeNumbers(3, 4, 5);
+
+        System.out.println("Task 12: Напишите метод, который получает на вход два целых числа и выводит случайное число в этом диапазоне.");
+        task12_getRnd(2, 7);
+
+        System.out.println("Task 13: Напишите метод, который выводит сумму двух случайных чисел от 1 до 6. (имитация броска кубиков)");
+        task13_rollTheDies();
+        task13_rollTheDies();
+        task13_rollTheDies();
+
+        System.out.println("Task 14: Напишите метод, который получает на вход число t и выводит в консоль результат выражения\n" +
+                " sin(2t) + sin(3t)");
+        task14_sin(5);
+
+        System.out.println("Task 15: Напишите метод, который получает на вход целочисленные m и d и проверяет, что введённые m-месяц и d-день, лежат в промежутке от 20.03 до 20.06.");
+        task15_checkDate(4, 15);
+
+        System.out.println("Task 16: Напишите метод, который получает на вход 3 целых числа, сравнивает их, и выводит в консоль \"Все три числа равны\" или \"не равны\".");
+        task16_compareThreeNumbers(1, 2, 3);
+        task16_compareThreeNumbers(3, 3, 3);
+
+        System.out.println("Task 17: Напишите метод, который проверяет, что переменные x и y типа double лежат строго в пределах от 0 до 1");
+        task17_checkDoubleInBounds(0.5d, 0.99d);
     }
+
+    private static void task17_checkDoubleInBounds(double x, double y) {
+        System.out.println( Double.compare(x, 0.0d) == 1 &&
+                            Double.compare(x, 1.0d) == -1 &&
+                        Double.compare(y, 0.0d) == 1 &&
+                        Double.compare(y, 1.0d) == -1);
+    }
+
 
     //Task 3: Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
     //где a, b, c, d – входные параметры этого метода;
@@ -100,5 +151,43 @@ public class Main {
         final boolean check4 = check1 && check2 || check1 && check3;
         String answer = check4 ? year + " - является високосным" : (year + " - не високосный");
         System.out.println(answer);
+    }
+
+    private static void task9_isTrue(boolean a, boolean b) {
+        System.out.println((!(a && b) && (a || b)) || ((a && b) || !(a || b)));
+    }
+
+    private static void task10_checkВivisibility(int a, int b) {
+        System.out.println(a % b == 0);
+    }
+
+    private static void task11_checkThreeNumbers(int a, int b, int c) {
+        System.out.println((a + b > c) && (a + c > b) && (a + c > b));
+    }
+
+    private static void task12_getRnd(int a, int b) {
+        System.out.println(Math.random() * (b - a) + a);
+    }
+
+    private static void task13_rollTheDies() {
+        System.out.println(new Random().nextInt(6) + new Random().nextInt(6) + 2);
+    }
+
+    private static void task14_sin(int t) {
+        System.out.println(Math.sin(2 * t) + Math.sin(3 * t));
+    }
+
+    private static void task15_checkDate(int m, int d) {
+        LocalDate date = LocalDate.of(2019, m, d);
+        System.out.println(
+                date.isAfter(LocalDate.of(2019, 3, 20)) &&
+                        date.isBefore(LocalDate.of(2019, 6, 20))
+        );
+
+    }
+
+    private static void task16_compareThreeNumbers(int a, int b, int c) {
+        boolean check = a == b && b == c;
+        System.out.println(check ? "Все три числа равны" : "не равны");
     }
 }
